c782dda1941c20d46da0759498745c453d17c45b
Brandon <BMHoutchens@gmail.com>
2019-02-12 11:50:45 -0500

    login/signup HTML and functionality stubbed out

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService} from "../services/auth.service";
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  title: string = "Login"
+  buttonTitle: string = "SignUp!"
+  bool: boolean = true;
+
+  toggleView() {
+    this.bool = !this.bool;
+    console.log(this.bool)
+    let login = document.getElementById("login");
+    let signup = document.getElementById("signup");
+    
+    if(this.bool == true){
+      login.style.display = "block";
+      signup.style.display = "none";
+      this.title = "Login"
+      this.buttonTitle = "SignUp!"
+    } else {
+      login.style.display = "none";
+      signup.style.display = "block";
+      this.title = "Signup"
+      this.buttonTitle = "Login!"
+    }
+  }
+
+  sendLogin(username: string, password: string): void{
+    event.preventDefault();
+    console.log(username, password);
+  }
+
+  sendSignup(firstName: string, lastName: string, email: string, username:string, password: string, confirmPassword: string): any {
+    event.preventDefault();
+
+    let warningDiv = document.getElementById("warningDiv");
+    let warning = document.getElementById("warning");
+
+    if(password != confirmPassword){
+      warningDiv.style.display = "block";
+      warning.innerText = "Your passwords do not match, please try again";
+      return
+    }
+    if(firstName == "" || lastName == "" || email == "" || username == "" || password == "" || confirmPassword == ""){
+      warningDiv.style.display = "block";
+      warning.innerText = "Please fill all fields before submitting";
+      return
+    }
+    console.log(firstName);
+  }
+
+  hideWarning(){
+    document.getElementById("warningDiv").style.display = "none";
+    document.getElementById("warning").innerText  = "";
   }
 
 }
